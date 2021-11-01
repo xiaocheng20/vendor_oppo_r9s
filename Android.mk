c7c836e36860c747601310c17c8b0fbe1ea1386c
xiaocheng20 <yuchengtan0@gmail.com>
2021-11-01 14:48:14 +0800

    drop fastdormancy.apk

--- Android.mk
+++ Android.mk
@@ -52,17 +52,6 @@ LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := fastdormancy
-LOCAL_MODULE_OWNER := oppo
-LOCAL_SRC_FILES := proprietary/app/fastdormancy/fastdormancy.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := shutdownlistener
 LOCAL_MODULE_OWNER := oppo
 LOCAL_SRC_FILES := proprietary/app/shutdownlistener/shutdownlistener.apk
